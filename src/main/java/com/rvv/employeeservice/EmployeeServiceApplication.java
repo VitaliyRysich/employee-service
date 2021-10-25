dcf47b5d3602103e0eec1e399d42c1c769e93ee3
Vitalii.Rysich <Vitalii.rysich@asseco.pl>
2021-10-25 23:27:13 +0200

    Remove init data

--- src/main/java/com/rvv/employeeservice/EmployeeServiceApplication.java
+++ src/main/java/com/rvv/employeeservice/EmployeeServiceApplication.java
@@ -15,33 +15,10 @@ import java.util.List;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Employees API", version = "3.0", description = "Employees Information"))
-public class EmployeeServiceApplication implements ApplicationRunner {
+public class EmployeeServiceApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(EmployeeServiceApplication.class, args);
     }
 
-
-    @Autowired
-    EmployeeRepository employeeRepository;
-
-    @Override
-    public void run(ApplicationArguments args) throws Exception {
-        Employee employee1 = Employee.builder()
-                .name("Vitalii")
-                .surname("Rysich")
-                .grade(Grade.EXPERT)
-                .salary(10000.0)
-                .build();
-
-        Employee employee2 = Employee.builder()
-                .name("Jan")
-                .surname("Kowalski")
-                .grade(Grade.NOVICE)
-                .salary(8000.0)
-                .build();
-
-        employeeRepository.save(employee1);
-        employeeRepository.save(employee2);
-    }
 }
